f5377ba97d507304810c9bdf5c82b832d5a034ee
Pushister <p.povolotskyi@gmail.com>
2023-01-05 20:53:07 +0200

    11. Docker Compose

--- worker.py
+++ worker.py
@@ -1,14 +1,23 @@
+import os
+
 from celery import Celery
 import datetime
 
 
-app = Celery('tasks', broker='pyamqp://guest@localhost//')
+rabbit_host = os.environ.get('RABBIT_HOST', 'localhost')
+app = Celery('worker', broker=f'pyamqp://guest@{rabbit_host}//')
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(10.0, add.s(x = 2,  y = 4), name='10 seconds')
 
 
 @app.task
 def add(x, y):
-    print (x+y)
+    print(x+y)
     with open('test.txt', 'w') as f:
         f.write(f'x+y = {x+y} {datetime.datetime.now()} ')
     return x + y
+
 
